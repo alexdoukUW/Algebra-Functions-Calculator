3e43ba3e17b8440bc94036a4efc754cf721cb894
alexdoukUW <119360646+alexdoukUW@users.noreply.github.com>
2024-05-04 00:29:56 -0700

    Add files via upload
    initial plan for now

---
+++ main.py
@@ -0,0 +1,42 @@
+nato_alphabet = {
+"A": "Alpha", "B": "Bravo", "C": "Charlie",
+"D": "Delta", "E": "Echo", "F": "Foxtrot",
+"G": "Golf", "H": "Hotel", "I": "India",
+"J": "Juliett", "K": "Kilo", "L": "Lima",
+"M": "Mike", "N": "November", "O": "Oscar",
+"P": "Papa", "Q": "Quebec", "R": "Romeo",
+"S": "Sierra", "T": "Tango", "U": "Uniform",
+"V": "Victor", "W": "Whiskey", "X": "X-ray",
+"Y": "Yankee", "Z": "Zulu"
+}
+
+# While loop to run to keep program running
+
+# User Input for Name
+
+# Sort Function
+# Sort through the name
+# Match letter to Nato_Alphabet Key
+# If it matches, add to a list called nato_alpha_name
+
+# Welcome user in their alpha name
+
+# Allow for options of what they could do
+# Option one - you can input two functions
+# A. Being Quadratic Function
+# Prompt if user wants to find the Domain
+# Prompt if user wants to find the Range
+# Prompt if the user wants to find the Inverse
+# B. sin/cos/tan functions
+# Prompt if the user wants to find the Amplitude
+# Prompt if the user wants to find the Period
+# Prompt if the user wants to find the Vertical Asymptote
+# Prompt if the user wants to find the Vertical Stretch/Compress
+# Prompt if the user wants to find the Horizontal Stretch/Compress
+# Option two - Clear Screen
+
+
+# MAKE IT INTO AN APP
+# ad of app is starts like on TikTok if it's still up "hey do you know to get better at your algebra skills" AI voice
+# with a video on the bottom for the short attention span mfs
+# inside of app every three problems is a skippable ad
